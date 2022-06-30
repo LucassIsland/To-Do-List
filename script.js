496790c03137c2ab63dd99af3b8bbb81c2aa1ae1
Lukasz Watroba <108336492+LucassIsland@users.noreply.github.com>
2022-06-30 12:41:19 +0200

    Add files via upload
    This is my final version 1.0 - my first app :)

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 'use strict';
 
+let choose;
+
 const clearFunction = function () {
-    console.clear();
-    console.log(menu);
-    choose = prompt(`Select your choice: `);
+  console.clear();
+  console.log(menu);
+  choose = prompt(`Select your choice: `);
 };
 
 const showTasks = function () {
-    console.clear();
-    console.log(`
+  console.clear();
+  console.log(`
 
 
 Tasks List:`);
-    for (let i = 0; i < tasks.length; i++) {
-        console.log(`${i + 1}. ${tasks[i]}`)
-    }
+  for (let i = 0; i < tasks.length; i++) {
+    console.log(`${i + 1}. ${tasks[i]}`);
+  }
 };
 
-const tasks = ['Workout', 'Learning', 'Shopping'];
+const tasks = [
+  'Workout',
+  'Learning',
+  'Shopping',
+  'Car wash',
+  'Reading books',
+  'Go for a walk',
+];
 
-const menu = (`
+const menu = `
 
 
 "To Do" List
@@ -27,50 +36,48 @@ const menu = (`
 1. Show Tasks
 2. Add Task
 3. Delete Task
-4. Quit`);
+4. Quit`;
 
 console.log(menu);
 
-let choose = prompt(`Select your choice: `);
-
 do {
-    if (choose == 1) {
-        showTasks();
-        const close = prompt(`Click "OK" for back to main menu`);
-    };
-    clearFunction();
+  if (choose == 1) {
+    showTasks();
+    const close = prompt(`Click "OK" for back to main menu`);
+  }
 
-    if (choose == 2) {
-        showTasks();
-        tasks.push(prompt(`Write new task which you want to add:`));
-        showTasks();
-        const close = prompt(`New task has added to the list on the last position.
+  if (choose == 2) {
+    showTasks();
+    tasks.push(prompt(`Write new task which you want to add:`));
+    showTasks();
+    const close = prompt(`New task has added to the list on the last position.
 Click "OK" for back to main menu`);
-    };
-    clearFunction();
+  }
 
-    if (choose == 3) {
-        showTasks();
-        const delTask = prompt(`Type the number of task you want to remove from the list:`);
+  if (choose == 3) {
+    showTasks();
+    const delTask = prompt(
+      `Type the number of task you want to remove from the list (choose proper number!):`
+    );
 
-        while (delTask == 0 || delTask > tasks.length) {
-            let delTask = prompt(`You write something wrong. Try again:`);
-        };
-
-        tasks.splice(delTask - 1, 1);
-        showTasks();
-        const close = prompt(`Your task has been deleted from the list.
-Click "OK" for back to main menu`);
-    };
-    clearFunction();
-}
-
-while (choose != 4);
+    if (delTask == 0 || delTask > tasks.length) {
+      const close = prompt(
+        `There's is no task of number ${delTask}, so you can't delete it. Click "OK" for back to main menu`
+      );
+    } else {
+      tasks.splice(delTask - 1, 1);
+      showTasks();
+      const close = prompt(`Your task has been deleted from the list.
+    Click "OK" for back to main menu`);
+    }
+  }
+  clearFunction();
+} while (choose != 4);
 
 if (choose == 4) {
-    console.clear();
-    console.log(`
+  console.clear();
+  console.log(`
     
     
 Thanks and see you again next time!`);
-};
+}
